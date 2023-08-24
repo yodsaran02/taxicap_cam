608e709f4cb2c7e1516718e6630b14998087b103
ark4660 <79065547+ark4660@users.noreply.github.com>
2023-08-24 04:17:11 +0000

    main.py alpha channel testing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 cam = cv2.VideoCapture(0)
 
 while True:
@@ -9,6 +9,16 @@ while True:
     	
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('gray',gray)
+    alpha = np.sum(frame, axis=-1) > 0
+
+    # Convert True/False to 0/255 and change type to "uint8" to match "na"
+    alpha = np.uint8(alpha * 255)
+
+    # Stack new alpha layer with existing image to go from BGR to BGRA, i.e. 3 channels to 4 channels
+    result = np.dstack((na, alpha))
+
+    # Save result   
+    cv2.imshow('alpha', result)
     key = cv2.waitKey(1)
     if key == 27:
         break
