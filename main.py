15ba48492d2548fbb356b4f6ec5d5a926c99e651
yodsaran02 <yodsaran02@gmail.com>
2023-08-24 15:15:39 +0000

    update main2

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
-cam = cv2.VideoCapture(0)
+#cam = cv2.VideoCapture(0)
 
-while True:
-    check, frame = cam.read()
+#while True:
+#check, frame = cam.read()
     
-    #cv2.imshow('video', frame)
-    import test_capture
-    import corner 
-    import line
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+#cv2.imshow('video', frame)
+import test_capture
+import corner 
+import line
+key = cv2.waitKey(1)
+if key == 27:
+    break
 
 cam.release()
 cv2.destroyAllWindows()
