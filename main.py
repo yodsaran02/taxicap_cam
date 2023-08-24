b83397a9c42d8e68d83c74b3729235fbc2c3f71c
yodsaran02 <yodsaran02@gmail.com>
2023-08-24 15:13:42 +0000

    update main

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ cam = cv2.VideoCapture(0)
 
 while True:
     check, frame = cam.read()
-
-    cv2.imshow('video', frame)
+    
+    #cv2.imshow('video', frame)
+    import test_capture
+    import corner 
+    import line
     key = cv2.waitKey(1)
     if key == 27:
         break
