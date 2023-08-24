f8f1677b5dc82f440519ac4d2faf46215b39f5b1
yodsaran02 <yodsaran02@gmail.com>
2023-08-24 10:18:22 +0000

    convert grayscale

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ while True:
     check, frame = cam.read()
 
     cv2.imshow('video', frame)
-
+    	
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray',gray)
     key = cv2.waitKey(1)
     if key == 27:
         break
