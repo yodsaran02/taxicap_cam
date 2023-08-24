60b20a7b2803dd1096f5296443411eb42dcf9294
yodsaran02 <yodsaran02@gmail.com>
2023-08-24 14:07:42 +0000

    update corner.py to detect realtime corner

--- corner.py
+++ corner.py
@@ -1,19 +1,20 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread('test.jpg')
+cam = cv.VideoCapture(0)
 
-cv.imshow('img', img)
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+while True:
+    chech, img = cam.read()
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-gray = np.float32(gray)
-dst = cv.cornerHarris(gray, 2, 3, 0.04)
+    gray = np.float32(gray)
+    dst = cv.cornerHarris(gray, 2, 3, 0.04)
 
-dst = cv.dilate(dst, None)
+    dst = cv.dilate(dst, None)
 
-img[dst > 0.01 * dst.max()] = [0, 0, 255]
+    img[dst > 0.01 * dst.max()] = [0, 0, 255]
 
-cv.imshow('dst', img)
+    cv.imshow('dst', img)
 
 if cv.waitKey(0) & 0xff == 27:
     cv.destroyAllWindows()
