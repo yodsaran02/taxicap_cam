d7cf258950bd1a60634bd5b6b318302dfa85a5d6
yodsaran02 <yodsaran02@gmail.com>
2023-08-25 11:20:14 +0700

    fix ocr 6

--- ocr.py
+++ ocr.py
@@ -1,24 +1,24 @@
-from PIL import Image, ImageOps
-import pytesseract
-import argparse
-import os
+import cv2
+import numpy as np
+import tensorflow as tf
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
-args = vars(ap.parse_args())
+# Load the pre-trained object detection model (TensorFlow Object Detection API)
+model = tf.saved_model.load('path_to_your_model')
 
-#Open the test image
-image = Image.open(args["image"])
+# Load and preprocess the image
+image = cv2.imread('image_path')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
+input_tensor = tf.convert_to_tensor(image)
 
-#Convert the image to grayscale
-gray = ImageOps.grayscale(image)
+# Run object detection
+detections = model(input_tensor)
 
-#Temporarily save the grayscale file
-filename = "{}.png".format(os.getpid())
-gray.save(filename)
+# Post-process the detections to get bounding boxes
+# Filter detections based on confidence threshold and class labels
 
-#Open the grayscale file and run OCR on it
-text = pytesseract.image_to_string(Image.open(filename))
-os.remove(filename)
-print(text)
+# Extract and save the numbers
+for detection in filtered_detections:
+    x, y, w, h = detection['bbox']
+    number_image = image[y:y+h, x:x+w]
+    cv2.imwrite('number_{}.png'.format(some_counter), number_image)
+    some_counter += 1
