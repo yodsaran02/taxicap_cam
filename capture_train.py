d53b1d2b439ae478cac24b2b109815def9e8a8d4
yodsaran02 <yodsaran02@gmail.com>
2023-08-25 12:45:52 +0700

    fix ocr 13

--- capture_train.py
+++ capture_train.py
@@ -6,7 +6,7 @@ time.sleep(1)
 while True:
     check, img = cam.read()
     cv2.imshow("feed",img)
-    if check and i%20 = 0:
+    if check and i%20 == 0:
         print("Saved",str(i))
         cv2.imwrite("trained/"+str(i)+".jpg",img)
     i += 1
